91171430a3370cf0ba4ba8c29fe94c493cce7871
Linda ben rhaim <lbenrhaim@gmail.com>
2025-10-14 15:46:17 +0200

    change generation type for user

--- src/main/java/com/foodify/server/modules/identity/domain/User.java
+++ src/main/java/com/foodify/server/modules/identity/domain/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Table(name = "app_users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String email;
